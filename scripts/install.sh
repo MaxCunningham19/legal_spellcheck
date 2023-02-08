dd79bb71527f559343c89870e7a1f3b31d7d50a0
Naza <uzoukwuc@tcd.ie>
2023-02-08 17:38:26 +0000

    Read: if node_modules exists and node_modules not in path

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,8 +10,8 @@ export DJANGO_PROJECT_ROOT="$(pwd)"
 source env/bin/activate
 # Add node binaries to PATH if they have not been added already. (if
 # they exist at all).
-([ -d client/node_modules ] && [[ $PATH =~ client/node_modules ]]) \
-    || PATH+=:$(realpath client/node_modules/.bin/)
+([ -d client/node_modules/.bin/ ] && [[ ! $PATH =~ client/node_modules ]]) \
+    && PATH+=:$(realpath client/node_modules/.bin/)
 # Define manage function that automatically loads the models we
 # use. Use this instead of 'python3 mange.py'. When ran without any
 # arguments, launches the shell.
