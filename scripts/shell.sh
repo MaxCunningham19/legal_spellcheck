4cf39f568cde0296bbbd4853e41a22d54ef35216
Naza <uzoukwuc@tcd.ie>
2023-02-08 17:08:15 +0000

    Add utility shell-launcher

---
+++ scripts/shell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Executing this will launch a shell in a running webserver container
+#that you started using 'docker compose up webserver-local'. It will
+#have the node binary locations placed in PATH and the VENV activation
+#script soruced.
+# 
+# Example Usage:
+#  > sh scripts/shell.sh
+#  $ (env) ....
+container=$(docker container list | awk '/webserver-local/ { print $1 }')
+docker container exec -it $container bash -c '. scripts/install.sh && exec bash -i'
