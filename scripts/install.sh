45b2cf0f64925cc54200bb33621e83c8e00c5562
Naza <uzoukwuc@tcd.ie>
2023-02-08 17:30:20 +0000

    Only try to add node modules to bin if node_modules exist in the first place

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,8 +3,10 @@ DJANGO_PROJECT_ROOT="$(dirname $(dirname $(realpath $0)))"
 cd "$DJANGO_PROJECT_ROOT"
 # Source VENV
 source env/bin/activate
-# Add node binaries to PATH if they have not been added already.
-[[ $PATH =~ client/node_modules ]] || PATH+=:$(realpath client/node_modules/.bin/)
+# Add node binaries to PATH if they have not been added already. (if
+# they exist at all).
+([ -d client/node_modules ] && [[ $PATH =~ client/node_modules ]])\
+    || PATH+=:$(realpath client/node_modules/.bin/)
 # Define manage function that automatically loads the models we
 # use. Use this instead of 'python3 mange.py'. When ran without any
 # arguments, launches the shell.
