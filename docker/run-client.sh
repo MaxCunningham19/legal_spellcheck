fb25518f01f9ce86f8110e731488b8703908e42a
Naza <uzoukwuc@tcd.ie>
2023-02-08 00:12:44 +0000

    Replace shell scripts with docker compose approach

--- docker/run-client.sh
+++
@@ -1,9 +0,0 @@
-docker run \
-       -e POSTGRES_HOST=${POSTGRES_HOST:-postgres} \
-       -e POSTGRES_USER=${POSTGRES_USER:-dev} \
-       -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-dev} \
-       -e POSTGRES_PORT=${POSTGRES_PORT:-5432} \
-       -e POSTGRES_DB=${POSTGRES_DB:-database} \
-       -it --rm ${DEBUG:+--entrypoint bash} \
-       --network sweng-net "$@" \
-       sweng-32-client
