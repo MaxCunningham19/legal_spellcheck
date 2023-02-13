552f6051e666c9fcf84387d8741047f0379d94ba
Naza <uzoukwuc@tcd.ie>
2023-02-13 20:58:06 +0000

    Don't be silent when the database isn't running

--- scripts/set-postgres-host.sh
+++ scripts/set-postgres-host.sh
@@ -3,6 +3,6 @@
 # This script sets the POSTGRES_HOST environment variable to the
 # IP Address of the running database container.
 container=$(docker container list | awk '/database/ { print $1 }')
-[ -z "$container" ] && echo No database container running... Doing nothing && return 1
+[ -z "$container" ] && >&2 echo No database container running... Doing nothing && return 1
 host=$(docker inspect -f '{{ range .NetworkSettings.Networks }}{{ .IPAddress }}{{end}}' $container)
 export POSTGRES_HOST=$host
