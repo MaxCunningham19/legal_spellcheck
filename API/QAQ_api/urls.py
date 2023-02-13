5d3a87764955c4b4db8d9caa6f3e4423a560f159
Naza <uzoukwuc@tcd.ie>
2023-02-13 20:27:14 +0000

    Restructure files

--- API/QAQ_api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.PostList.as_view()),
-    path('<int:pk>/', views.PostDetail.as_view()),]
