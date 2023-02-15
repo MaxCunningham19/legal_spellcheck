e3819a2a7fc1d44f33f21ce3602c22b723e57dd1
Max Romagnoli <maxxromagnoli@gmail.com>
2023-02-15 14:16:01 +0000

    Removed React Native components bc not used dependencies

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,29 +1,17 @@
 import React, {useState} from 'react'
-import { Button, ButtonGroup } from "./Button";
-import { Button2 } from './Button'
+import Button from "./Button";
 import styles from './Navbar.module.css'
-import "rsuite/dist/rsuite.min.css";
-
-
-function sayHello() {
-  alert('You clicked me!');
-}
 
 function Navbar() {
   return (
     <>   
         <nav className={styles['Navbar']}>
             <div className={styles['navbar-container']}>
-               <Button color="cyan" appearance="primary" block="true">My Documents </Button> 
 
-              
             </div>
         </nav>
     </>
   )
 }
-
-<Button onClick={sayHello}>Default</Button>
-
 
 export default Navbar
