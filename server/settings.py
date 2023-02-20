804ce0d9576e470ea624023949d28e0271851163
Naza <uzoukwuc@tcd.ie>
2023-02-20 16:33:30 +0000

    Add Django scaffolding to serve react app

--- server/settings.py
+++ server/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'client.apps.ClientConfig',
     'api.apps.ApiConfig',
     'rest_framework'
 ]
