48853105dc1d95dd052582af7b1b31001ba385a7
Max Romagnoli <maxxromagnoli@gmail.com>
2023-02-24 18:28:04 +0000

    Update Carousel to fetch data from external file

--- client/src/components/Editor.js
+++ client/src/components/Editor.js
@@ -2,13 +2,17 @@ import React, {useState} from 'react'
 import { Button } from './Button'
 import Carousel from './Carousel'
 import styles from './Editor.module.css'
+import documentData from '../data/documentsData.json' 
 
 function Editor() {
   return (
     <>
       <section className={styles['Editor']}>
         <div className={styles['editor-container']}>
-          <Carousel className={styles['Carousel']} />
+          <Carousel 
+            className={styles['Carousel']} 
+            data={documentData.documents[0]}    // TODO: defaulted to 0
+          />
         </div>
       </section>
     </>
