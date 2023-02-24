aace51be76576d9ffbc2e0852ad5842f16f137cc
Dylan Thompson <thompsdy@tcd.ie>
2023-02-24 11:29:23 +0000

    Kendra Integration

--- api/spellcheck.py
+++ api/spellcheck.py
@@ -5,6 +5,7 @@ consistency checking .etc.
 """
 
 import subprocess
+import boto3
 
 class Mistake():
     def __init__(self, word: str, location: int, suggestions: list[str]):
@@ -34,26 +35,30 @@ class Mistake():
 def check(content: str) -> list[Mistake]:
     """
     Takes a string and returns an array of mistake objects representing
-    the errorrs in that string.
+    the errors in that string.
     """
-    
-    raw_output = subprocess.run(
-        ["aspell", "-a"],
-        text=True,
-        input=content,
-        stdout=subprocess.PIPE
-    ).stdout
-    
+
+    kendra = boto3.client("kendra", aws_access_key_id='something',
+                          aws_secret_access_key='somethign',
+                          region_name='eu-west-1')
+    index_id = "something"
+    max_suggestions = 5
+
     mistakes = []
-    
-    for line in raw_output.splitlines()[1:]:
-        if len(line) == 0:
-            pass
-        elif line[0] == "&":
-            word, number_of_suggestions, location, *suggestions = line.split()[1:]
-            mistakes.append(Mistake(word, int(location.rstrip(":")), suggestions))
-        elif line[0] == "#":
-            word, location = line.split()[1:]
-            mistakes.append(Mistake(word, int(location.rstrip(":")), []))
+
+    # Split the content into words
+    words = content.split()
+
+    # Loop through each word and retrieve query suggestions
+    for i, word in enumerate(words):
+        response = kendra.get_query_suggestions(
+            IndexId=index_id,
+            QueryText=word,
+            MaxSuggestionsCount=max_suggestions
+        )
+
+        # Check if the word has any suggestions
+        if not response['Suggestions']:
+            mistakes.append(Mistake(word, sum(len(w) + 1 for w in words[:i]), []))
 
     return mistakes
