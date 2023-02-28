d0548495051da34a54bc6aa3f97fa499d5c857e7
Ignatii Tsitsenko <tsitseni@tcd.ie>
2023-02-28 13:49:22 +0000

    Url and view changes

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import (get_document_blocks, check_document_blocks, DocumentList)
+from .views import (get_document_blocks, check_document_blocks, DocumentList, add_documents)
 
 app_name = 'api'
 
 urlpatterns = [
     path('document/', DocumentList.as_view(), name='get_documents'),
     path('document/<int:pk>', get_document_blocks, name='get_document_blocks'),
-    path('check/document/<int:pk>', check_document_blocks, name='check_document_blocks')
-]
+    path('check/document/<int:pk>', check_document_blocks, name='check_document_blocks'),
+    path('add/', add_documents, name='add_documents')]
--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ import rest_framework
 from django.shortcuts import render, get_list_or_404
 from rest_framework import generics
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Document, Block
 from .serializers import DocumentSerializer, BlockSerializer, MistakeSerializer
 
@@ -17,11 +18,18 @@ class DocumentDetail(generics.RetrieveAPIView):
 def get_document_blocks(request, pk):
     blocks = Block.objects.filter(block_document=pk)
     serializer = BlockSerializer(blocks, many=True)
-    return rest_framework.response.Response(serializer.data, status=200)
+    return Response(serializer.data, status=200)
 
 @api_view()
 def check_document_blocks(request, pk):
     mistakes = [dict(block_order=block.block_order,
                      mistakes=MistakeSerializer(block.spellcheck(), many=True).data) \
                 for block in Block.objects.filter(block_document=pk)]
-    return rest_framework.response.Response(mistakes, status=200)
+    return Response(mistakes, status=200)
+
+@api_view()
+def add_documents(request):
+    serializer = DocumentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
