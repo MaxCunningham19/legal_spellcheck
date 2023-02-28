d64e608e0c53790dbd313c371e1d1dc934354bef
Naza <uzoukwuc@tcd.ie>
2023-02-28 21:24:01 +0000

    run updates in paralell

--- scripts/force-local-refresh.sh
+++ scripts/force-local-refresh.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 . scripts/install.sh
 # Copy updated static files
-python3 manage.py collectstatic --no-input
+python3 manage.py collectstatic --no-input & collect_pid=$!
 # Recompile javascript bundle
-cd client; webpack --stats errors-warnings --mode development
+cd client; webpack --stats errors-warnings --mode development & webpack_pid=$!
 # Restart httpd
-1>/dev/null pgrep httpd && httpd -k restart || httpd -k start
+(1>/dev/null pgrep httpd && httpd -k restart || httpd -k start) & httpd_pid=$!
+
+wait $collect_pid $webpack_pid $httpd_pid
