83f6e29589f0bb509f1511219b9e61b94209f9c2
Naza <uzoukwuc@tcd.ie>
2023-02-28 20:36:02 +0000

    Make less ugly

--- scripts/mount-file-watch.sh
+++ scripts/mount-file-watch.sh
@@ -6,9 +6,14 @@ touch $watched_files
 
 # Get all files for which there is no watch
 unwatched_files=$(mktemp)
-cat <(find client server api -type f -regex '.*\.\(py\|js\|html\|svg\|css\)$' \
-          | grep -Ev '#|node_modules') $watched_files \
-    | sort | uniq -u | sed '/^$/d' > $unwatched_files
+cat <(find client server api \
+           -type f \
+           -regex '.*\.\(py\|js\|html\|svg\|css\)$' \
+          | grep -Ev '#|node_modules') \
+    $watched_files \
+    | sort \
+    | uniq -u \
+    | sed '/^$/d' > $unwatched_files
 
 # Add new files 
 cat $unwatched_files >> $watched_files
@@ -16,7 +21,7 @@ cat $unwatched_files >> $watched_files
 # Spawn a new watch for the files not included in the old watch.
 if [ $(wc -l $unwatched_files | cut -f1 -d' ') -gt 0 ]
 then
-    echo New files added: $(cat $unwatched_files | head -n 10) ...
+    echo New files added: $(head -n 10 $unwatched_files) ...
     sed 's/$/:c/' $unwatched_files | xargs inotifyd $(realpath scripts/on-change.sh) &
 else
     echo No new files added in directory change
