ffef3220be0737a9f393e64636c56aed3a43c45e
QimingNie <123891635+QimingNie@users.noreply.github.com>
2023-03-01 15:14:25 +0000

    add put post delete document in views.py
    add put post delete document in views.py

---
+++ views.py
@@ -0,0 +1,55 @@
+import rest_framework
+from django.shortcuts import render, get_list_or_404
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from .models import Document, Block
+from .serializers import DocumentSerializer, BlockSerializer, MistakeSerializer
+
+class DocumentList(generics.ListAPIView):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+
+@api_view()
+def post_document(request):
+    serializer = BlockSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
+
+class DocumentDetail(generics.RetrieveAPIView):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+
+
+@api_view()
+def get_document_blocks(request, pk):
+    blocks = Block.objects.filter(block_document=pk)
+    serializer = BlockSerializer(blocks, many=True)
+    return rest_framework.response.Response(serializer.data, status=200)
+
+@api_view()
+def put_document_blocks(request, pk):
+    block = block.objects.get(pk=pk)
+    serializer = DocumentSerializer(block, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return rest_framework.response.Response(serializer.data, status=200)
+    return rest_framework.response.Response(serializer.errors, status=400)
+
+@api_view()
+def delete_document_blocks(request, pk):
+    block = block.get_object(pk)
+    block.delete()
+    return rest_framework.response.Response(status=204)
+
+@api_view()
+def check_document_blocks(request, pk):
+    mistakes = [dict(block_order=block.block_order,
+                     mistakes=MistakeSerializer(block.spellcheck(), many=True).data) \
+                for block in Block.objects.filter(block_document=pk)]
+    return rest_framework.response.Response(mistakes, status=200)
+
+
+
