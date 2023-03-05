31c2291cbed6a72ff9330997cb0dd6f03651e736
Max Romagnoli <maxxromagnoli@gmail.com>
2023-03-05 01:52:42 +0100

    Refactor to simplify interface and navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
-import { Browse } from "./pages/Browse";
+import { EditorPage } from "./pages/EditorPage";
 import { MyDocuments } from "./pages/MyDocuments";
 import styles from './App.module.css';
 import axios from "axios";
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Browse />} />
+        <Route path="/editor" element={<EditorPage />} />
         <Route path="/my-documents" element={<MyDocuments />} />
       </Routes>
     </>
