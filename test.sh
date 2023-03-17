a3cca2b71b4f7428a236ca57c9ce6c25c25da24c
ni-sauvage <sauvagen@tcd.ie>
2023-03-17 15:11:24 +0000

    Removing conflicting files to allow automatic merge

--- test.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p static/client/js
-touch static/client/js/main.js
-cd docker
-sudo docker-compose build
-sudo docker-compose up -d webserver
-sudo docker-compose exec -T webserver bash -c '. scripts/install.sh; python3 manage.py test'
-sudo rm -rf ../static
-sudo docker-compose down
