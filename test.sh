106bbc8dc74a26a183886922dac188b6eadf668a
Ignat Desktop <ignat77778@gmail.com>
2023-03-29 18:16:15 +0100

    Adding more mock data with a script

--- test.sh
+++ test.sh
@@ -6,3 +6,4 @@ sudo docker-compose up -d webserver
 sudo docker-compose exec -T webserver bash -c '. scripts/install.sh; python3 manage.py test'
 sudo rm -rf ../static
 sudo docker-compose down
+curl -X POST -H 'Content-Type: application/json' --data "@client/src/data/FILENAME.json" localhost:8080/api/documents/ > debug.html
