52840b2cfef607a820aa2db20b88821b8be781c2
Max Cunningham <maxzinedine@gmail.com>
2023-04-07 10:26:56 +0100

    Added constants, formatted code, reduced comments

--- api/spellcheck.py
+++ api/spellcheck.py
@@ -2,28 +2,30 @@ import json
 import time
 import requests
 import os
+import spellcheck_constants as const
+
 
 class Mistake():
     def __init__(self, word: str, location: int, suggestions: list[str]):
         self.word = word
-        """
+        '''
         The actual word that was misspelt.
-        """
+        '''
 
         self.start = location
-        """
+        '''
         The character index at which the mistake starts.
-        """
+        '''
 
         self.end = self.start + len(word)
-        """
+        '''
         The character index at which the mistake ends (exclusive).
-        """
+        '''
 
         self.suggestions = suggestions
-        """
+        '''
         The suggestions returned by Azure Bing Spell Check.
-        """
+        '''
 
     def __repr__(self) -> str:
         return str(self.__dict__)
@@ -35,58 +37,62 @@ def get_key():
 
 
 def check(content: str) -> list[Mistake]:
-    """
+    '''
     Takes a string and returns an array of mistake objects representing
     the errors in that string.
-    """
-    time.sleep(0.33334)     # Limits us to our rate on MS Azure
-    endpoint = 'https://api.bing.microsoft.com/v7.0/spellcheck'  # barely changes so its a constant but you can make it dynamic
-    api_key = get_key() # get it from azure key vault (should be converted to str)
+    '''
+    time.sleep(0.33334)  # Limits us to our rate on MS Azure
+    api_key = get_key()  # get key from Key Vault or ENV variables
 
-    mistakes = [] # mistakes array
-    mistakes_position = [] # position of the mistakes
-    object_mistakes = [] # an array of the object mistakes
-    suggestions = [] # suggestions array
+    # initalise variables to format response from Spellcheck API
+    mistakes = []
+    mistakes_position = []  # position of the mistakes
+    object_mistakes = []
+    suggestions = []
     sugg = []
-    position = [] # used for for loop
-    count = 0 # used for indexing
-    
+    position = []  # used for for loop
+    count = 0  # used for indexing
+
     # Set query parameters (required for the POST request to the spell check endpoint)
     params = {
-        "mkt": "en-US",
-        "mode": "proof",
-        "preContextText": "",
-        "postContextText": "",
+        'mkt': 'en-US',
+        'mode': 'proof',
+        'preContextText': '',
+        'postContextText': '',
     }
 
-    data = {"text": content} # this is where the data is being passed in from 'content' and sent as a payload through the POST request
+    data = {'text': content}  # format json for API
 
-    # Set headers (also essential to the spellcheck api)
+    # Set required headers
     headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Ocp-Apim-Subscription-Key": api_key,
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Ocp-Apim-Subscription-Key': api_key,
     }
-    
-    # Send request
-    response = requests.post(endpoint, headers=headers, params=params, data=data) # The actual request
-    json_response = response.json() # parse as JSON
 
-    for token in json_response['flaggedTokens']:    # loop through the objects
-        if token['type'] == 'UnknownToken':
-            mistakes.append(token['token']) # add it to the mistakes list
-            mistakes_position.append(token['offset'])
+    # Send request & parse as JSON
+    response = requests.post(const.BING_ENDPOINT, headers=headers,
+                             params=params, data=data)
+    json_response = response.json()
 
-        for suggestion in token["suggestions"]:
-                sugg.append([suggestion])
-        suggestions.append(sugg) 
-        position.append(count) # just putting numbers in eg [1, 2, 3, 4, ...] based on number of mistakes
-        count+=1       
-        i = 0
-                
+    for token in json_response[const.FLAGGED_TOKENS]:    # loop through mistakes
+        if token[const.MISTAKE_TYPE] == const.UNKOWN_TOKEN:
+            mistakes.append(token[const.TOKEN_VALUE])
+            mistakes_position.append(token[const.TOKEN_RELATIVE_POSITION])
+
+        for suggestion in token[const.SUGGESTIONS]:
+            sugg.append([suggestion])
+        suggestions.append(sugg) # just putting numbers in eg [1, 2, 3, 4, ...] based on number of mistakes
+        position.append(count)
+        count += 1
 
     for count in position:
-        accident = Mistake(mistakes[count], mistakes_position[count], suggestions[count]) # creates object mistakes
-        object_mistakes.append(accident) # creates an array of mistakes to return
-    
-    return object_mistakes
+        # creates object mistakes
+        accident = Mistake(
+            mistakes[count],
+            mistakes_position[count],
+            suggestions[count]
+        )
+        # creates an array of mistakes to return
+        object_mistakes.append(accident)
 
+    return object_mistakes
